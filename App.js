a3be5b8e4ce717689545bc5802f9c161e0669dca
hp <ichigo2297@gmail.com>
2018-04-09 18:15:49 +0630

    native-base and navigation ok

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import {
+  Container,
+  Header,
+  Left,
+  Right,
+  Body,
+  Title,
+  Text,
+  Content
+} from 'native-base';
+import { AppLoading } from 'expo';
 
-export default class App extends React.Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = { loading: true };
+  }
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+    });
+    this.setState({ loading: false });
+  }
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+    if (this.state.loading) {
+      return <AppLoading/>
+    }
+    return(
+      <Container>
+        <Header>
+          <Left/>
+          <Body>
+            <Title>Strix</Title>
+          </Body>
+          <Right/>
+        </Header>
+        <Content>
+          <Text>This is a test for native-base</Text>
+        </Content>
+      </Container>
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+export default StackNavigator({
+  Home: {
+    screen: App,
   },
 });
