b6436e70545eb3174c0ff889e3ace5208ed3b482
hp <ichigo2297@gmail.com>
2018-05-09 12:53:44 +0630

    qr scanner ok

--- app/booster/qrscanner.js
+++ app/booster/qrscanner.js
@@ -1,53 +1,84 @@
-import React, { Component } from 'react';
-import { Text, View, StyleSheet, Alert } from 'react-native';
-import { Constants, BarCodeScanner, Permissions } from 'expo';
+import React, { PureComponent } from 'react';
+import { View, StyleSheet, TouchableOpacity, Alert } from 'react-native';
+import ViewFinder from 'react-native-view-finder'
+import { BarCodeScanner, Permissions } from 'expo'
+import { Container, Text } from 'native-base'
+import { MaterialIcons, Ionicons } from '@expo/vector-icons'
 
-export default class QRScanner extends Component {
-  state = {
-    hasCameraPermission: null
-  };
+export default class QRScanner extends PureComponent {
+  constructor(props) {
+    super(props)
 
-  componentDidMount() {
-    this._requestCameraPermission();
+    this.state = {
+      hasCameraPermssion: null,
+      isBack: true,
+      isTorchOn: false,
+      data: '',
+      type: ''
+    }
   }
 
-  _requestCameraPermission = async () => {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({
-      hasCameraPermission: status === 'granted',
-    });
-  };
+  componentDidMount() {
+    this._askCameraPermission()
+  }
 
-  _handleBarCodeRead = data => {
+  _askCameraPermission = async() => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA)
+    this.setState({ hasCameraPermssion: status === 'granted' })
+  }
+
+  _handleBarCodeRead = (data) => {
     Alert.alert(
       'Scan successful!',
       JSON.stringify(data)
     );
-  };
+  }
+
+  _handleReverseCamera = () => {
+    this.setState({ isBack: !this.state.isBack })
+  }
+
+  _handleTorch = () => {
+    this.setState({ isTorchOn: !this.state.isTorchOn })
+  }
 
   render() {
-    return (
-      <View style={styles.container}>
-        {this.state.hasCameraPermission === null ?
-          <Text>Requesting for camera permission</Text> :
-          this.state.hasCameraPermission === false ?
-            <Text>Camera permission is not granted</Text> :
-            <BarCodeScanner
-              onBarCodeRead={this._handleBarCodeRead}
-              style={{ height: 400, width: 300 }}
-            />
-        }
-      </View>
-    );
+    const { hasCameraPermssion, isBack, isTorchOn } = this.state
+
+    return(
+      <Container>
+        <BarCodeScanner
+          type={isBack ? 'back' : 'front'}
+          torchMode={isTorchOn ? 'on' : 'off'}
+          barCodeTypes={[BarCodeScanner.Constants.BarCodeType.qr]}
+          onBarCodeRead={this._handleBarCodeRead}
+          style={StyleSheet.absoluteFill}
+        />
+        <ViewFinder />
+        <TouchableOpacity onPress={this._handleReverseCamera} style={[style.btn, {right: 20}]}>
+          <Ionicons 
+            name="md-reverse-camera"
+            size={25} color="#37474F" />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this._handleTorch} style={[style.btn, {left: 20}]}>
+          <MaterialIcons 
+            name={isTorchOn ? 'flash-on' : 'flash-off'}
+            size={25} color="#37474F" />
+        </TouchableOpacity>
+      </Container>
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+const style = StyleSheet.create({
+  btn: {
+    position: 'absolute',
+    width: 60,
+    height: 60,
     alignItems: 'center',
     justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
+    borderRadius: 25,
+    backgroundColor: '#fff',
+    bottom: 20
   }
-});
+})
