76683091a8cd0130344f75b7fdc82122a8e288eb
hp <ichigo2297@gmail.com>
2018-07-19 22:38:46 +0630

    event & activity updated

--- app/event/index.js
+++ app/event/index.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
-import { View, TouchableHighlight } from 'react-native';
-import { Container, Content } from 'native-base'
+import { 
+    View, TouchableHighlight, TouchableOpacity, FlatList, ImageBackground, StyleSheet, Dimensions
+} from 'react-native';
+import { Container, Content, Text } from 'native-base'
 import { StackNavigator } from 'react-navigation'
+import { Row, Col } from 'react-native-easy-grid'
+import moment from 'moment'
 import { Ionicons } from '@expo/vector-icons'
 import { map } from '../partials/icons'
 import Sheader from '../partials/sheader'
@@ -11,24 +15,32 @@ import Style from '../style'
 import firebaseService from '../service/firebase'
 
 const FIREVENT = firebaseService.database().ref('events')
+const WIDTH = Dimensions.get('screen').width / 1.05
+const HEIGHT = Dimensions.get('screen').height / 4
 
 class Event extends Component {
     constructor(props) {
         super(props)
 
-        this.state = {
-            events: []
-        }
+        this.state = { events: [] }
+        this.generateRandomNum.bind(this)
     }
 
+    componentDidMount() {
+        this._getEvents()
+    }
+
+    generateRandomNum = () => {
+        return Math.floor(Math.random() * 100)
+    }
 
     _getEvents = async() => {
         let arr = []
         await FIREVENT.once('value', (snapshot) => {
-            snapshot.forEach(snap => { arr.push(snap.val()) })
+            snapshot.forEach(snap => { arr.push(snap) })
         }).then(() => {
             this.setState({ events: arr })
-        })
+        }).catch(err => alert(err))
     }
 
     render() {
@@ -37,9 +49,26 @@ class Event extends Component {
                 <Sheader navigation={this.props.navigation} />
                 <Bar />
                 <View style={{ flex: 1 }}>
-                    <Content>
-                        
-                    </Content>
+                    <FlatList
+                        data={this.state.events}
+                        renderItem={({item}) => (
+                            <TouchableOpacity id={item.key}>
+                                <ImageBackground source={{ uri: item.val().bgimg }} style={style.bgImg} borderRadius={8} >
+                                    <Col>
+                                        <Row size={65}></Row>
+                                        <Row size={35} style={style.bgTxt}>
+                                            <View style={{ marginLeft: 15 }}>
+                                                <Text style={Style.red}>
+                                                    {moment(item.val().datetime).format('ddd, MMM Do, YYYY @ h A')}
+                                                </Text>
+                                                <Text style={{ fontWeight: 'bold' }}>{item.val().title}</Text>
+                                            </View>
+                                        </Row>
+                                    </Col>
+                                </ImageBackground>
+                            </TouchableOpacity>
+                        )}
+                    />
                     <TouchableHighlight style={Style.fab} 
                         onPress={() => this.props.navigation.navigate('Map')}>
                         <Ionicons name={map} color='#fff' size={30} />
@@ -49,6 +78,24 @@ class Event extends Component {
         )
     }
 }
+
+const style = StyleSheet.create({
+    bgImg: {
+        width: WIDTH,
+        height: HEIGHT,
+        margin: 10,
+        padding: 0,
+        elevation: 7,
+        shadowColor: '#000',
+        shadowOffset: { width: 2, height: 4 }
+    },
+    bgTxt: {
+        backgroundColor: 'rgba(255, 255, 255, 0.95)',
+        alignItems: 'center',
+        borderBottomLeftRadius: 8,
+        borderBottomRightRadius: 8
+    }
+})
 
 const EventStacker = StackNavigator(
     {
--- app/profile/activity.js
+++ app/profile/activity.js
@@ -1,38 +1,96 @@
 import React, { Component } from 'react';
 import { Content, Text, Card, CardItem, View } from 'native-base';
-import { LineChart, YAxis, Grid } from 'react-native-svg-charts'
+import { 
+    LineChart, YAxis, Grid, ProgressCircle, PieChart
+} from 'react-native-svg-charts'
+import firebaseService from '../service/firebase'
+
+const FIREUSER = firebaseService.database().ref('users')
 
 export default class Activity extends Component {
     constructor(props) {
         super(props)
+
+        this.state = { points: 0, uId: firebaseService.auth().currentUser.uid }
+    }
+
+    componentDidMount() {
+        this._getUserPoints()
+    }
+
+    _getUserPoints = async() => {
+        await FIREUSER.child(this.state.uId).once('value', (snapshot) => {
+            this.setState({ points: snapshot.val().points })
+        })
     }
 
     render() {
         const data = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ]
+        const data2 = [ 50, 10, 40, 95 ]
         const contentInset = { top: 20, bottom: 20 }
+
+        const randomColor = () => ('#' + (Math.random() * 0xFFFFFF << 0).toString(16) + '000000').slice(0, 7)
+        const pieData = data2
+            .filter(value => value > 0)
+            .map((value, index) => ({
+                value,
+                svg: {
+                    fill: randomColor(),
+                    onPress: () => console.log('press', index),
+                },
+                key: `pie-${index}`,
+            }))
+
         return(
-            <Card>
-                <View style={{ height: 200, flexDirection: 'row', margin: 5 }}>
-                    <YAxis
-                        data={ data }
-                        contentInset={ contentInset }
-                        svg={{
-                            fill: 'grey',
-                            fontSize: 10,
-                        }}
-                        numberOfTicks={ 10 }
-                        formatLabel={ value => `${value}ÂºC` }
+            <Content padder>
+                <Card>
+                    <CardItem header> 
+                        <Text>Points + Status</Text>
+                    </CardItem>
+                    <CardItem> 
+                        <Text style={{ fontSize: 21, color: '#d32f2f' }}>{this.state.points} XP</Text>
+                    </CardItem>
+                    <ProgressCircle
+                        style={{ height: 100, marginBottom: 15 }}
+                        progress={ 0.4 }
+                        progressColor={'#2a4ff0'}
                     />
-                    <LineChart
-                        style={{ flex: 1, marginLeft: 16 }}
-                        data={ data }
-                        svg={{ stroke: 'rgb(134, 65, 244)' }}
-                        contentInset={ contentInset }
-                    >
-                        <Grid/>
-                    </LineChart>
-                </View>
-            </Card>
+                </Card>
+                <Card>
+                    <CardItem header>
+                        <Text>Achievement Track</Text>
+                    </CardItem>
+                    <View style={{ height: 250, flexDirection: 'row', margin: 10 }}>
+                        <YAxis
+                            data={ data }
+                            contentInset={ contentInset }
+                            svg={{
+                                fill: '#000',
+                                fontSize: 10,
+                            }}
+                            numberOfTicks={ 16 }
+                            formatLabel={ value => `${value}` }
+                        />
+                        <LineChart
+                            style={{ flex: 1, marginLeft: 16 }}
+                            data={ data }
+                            svg={{ stroke: '#2a4ff0' }}
+                            contentInset={ contentInset }
+                        >
+                            <Grid/>
+                        </LineChart>
+                    </View>
+                </Card>
+                <Card>
+                    <CardItem header>
+                        <Text>By Cluster</Text>
+                    </CardItem>
+                    <PieChart
+                        style={{ height: 200, margin: 15 }}
+                        data={ pieData }
+                    />
+                </Card>
+            </Content>
         )
     }
 }
